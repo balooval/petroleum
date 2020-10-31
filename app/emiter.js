b66045af8bb4d56490b9bc42076545251991ed74
Valère Dunand <valere.d@gmail.com>
2020-10-31 19:21:58 +0100

    Restrict emitter movements

--- app/emiter.js
+++ app/emiter.js
@@ -1,6 +1,6 @@
+import * as Input from './input.js';
 import * as ParticleStore from './particleStore.js';
 import * as Renderer from './renderer.js';
-import * as Input from './input.js';
 
 let mesh = null;
 let moveLeft = 0;
@@ -11,7 +11,8 @@ let shootFreq = 1;
 let shootLastStep = -999;
 let shootDirUpAdd = false;
 let shootDirUpRem = false;
-let shootAngle = -1;
+let shootAngle = -2;
+let positionX = 0;
 
 export let velocity = 0;
 
@@ -19,12 +20,11 @@ export function init() {
 	Input.evt.addEventListener('DOWN', null, onKeyDown); 
 	Input.evt.addEventListener('UP', null, onKeyUp); 
 	mesh = new THREE.Mesh(new THREE.BoxGeometry( 2, 2, 6 ), new THREE.MeshBasicMaterial({color:0x502020}));
-	mesh.position.z = 12;
+	mesh.position.z = -12;
 	Renderer.scene.add(mesh);
 }
 
 export function onKeyUp(_evt) {
-	// console.log('onKeyUp', _evt);
 	if (_evt == 'LEFT') {
 		moveLeft = 0;
 	} else if (_evt == 'RIGHT') {
@@ -39,7 +39,6 @@ export function onKeyUp(_evt) {
 }
 
 export function onKeyDown(_evt) {
-	// console.log('onKeyDown', _evt);
 	if (_evt == 'SPACE') {
 		isShooting = true;
 	} else if (_evt == 'LEFT') {
@@ -60,8 +59,12 @@ export function update(_step) {
 	if (shootDirUpRem) {
 		shootAngle -= 0.04;
 	}
-	velocity = (moveRigth - moveLeft) * speed;
-	mesh.position.x += velocity;
+	shootAngle = Math.max(shootAngle, -3);
+	shootAngle = Math.min(shootAngle, -1.8);
+	velocity = (moveLeft - moveRigth) * speed;
+	positionX += velocity;
+	positionX = Math.min(Math.max(positionX, -25), 25);
+	mesh.position.x = positionX;
 	if (isShooting) {
 		shoot(_step);
 	}
@@ -76,9 +79,10 @@ function shoot(_step) {
 
 function emit() {
 	let particle;
+	// console.log('shootAngle', shootAngle);
 	for (let i = 0; i < 2; i ++) {
-		particle = ParticleStore.get(mesh.position);
-		particle.setVelocity(shootAngle, 1.5, 0);
+		particle = ParticleStore.get(mesh.position.clone());
+		particle.setVelocity(shootAngle, -1.5, 0);
 		particle.start();
 	}
 }
